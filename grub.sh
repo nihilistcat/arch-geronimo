124d4b6a5ad0d5afa721beda9287bd79253b3d3b
nihilistcat <nihilistcat@users.noreply.github.com>
2013-12-25 01:32:26 -0500

    Update grub.sh
    new updated grub installation

--- grub.sh
+++ grub.sh
@@ -2,7 +2,6 @@
 
 # grub stuff
 
-pacman --noconfirm -S grub-bios
-grub-install targethdd
-cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+pacman --noconfirm -S grub
 grub-mkconfig -o /boot/grub/grub.cfg
+grub-install targethdd
