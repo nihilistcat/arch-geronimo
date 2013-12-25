64011dbf6ed788cc4b13377aebf4f77312933b9c
nihilistcat <nihilistcat@users.noreply.github.com>
2013-12-25 02:02:33 -0500

    temporary fix for arch bug FS#38234
    see comment https://bugs.archlinux.org/task/38234
    this is a temporary workaround

--- grub.sh
+++ grub.sh
@@ -3,5 +3,6 @@
 # grub stuff
 
 pacman --noconfirm -S grub
+echo "GRUB_DISABLE_SUBMENU=y" >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 grub-install --target=i386-pc --recheck targethdd
