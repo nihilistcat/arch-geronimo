4ebd7f2b8f2cc4a2c58a81c6bcda90ea3c93b51d
nihilistcat <nihilistcat@riseup.net>
2013-12-25 20:59:52 -0500

    uodated for settings module

---
+++ README.md
@@ -0,0 +1,5 @@
+*simple install script for arch linux written in bash*
+This is a project for me to learn bash, it's noobishly written but works fine for me.
+The current version is able to install arch linux to a MBR harddrive with one root and one swap partition and it uses the grub boot loader.
+You can set up a primary user with sudo privileges alongside the root account, and install opensource/proprietary drivers / desktop environments / synaptics etc.
+There is no support for advanced features like UEFI/GPT. But, due to the modular structure a simple script can be written to achieve this task.
--- settings.sh
+++ settings.sh
@@ -1,18 +1,55 @@
 #!/bin/bash
 
-echo "This script will install several personal settings for your system"
-select yn in "Proceed" "Exit"; do
+echo "Do you want a desktop?"
+select yn in "Yes" "No"; do
     case $yn in
-        Proceed ) sleep 2; break;;
-        Exit ) exit;;
+        Yes ) echo "installing xorg and synaptics drivers" ;
+			pacman --noconfirm -S mesa xorg-server xorg-xinit xf86-input-synaptics xf86-input-mouse ;
+			echo "Choose a Desktop Environtment"
+			options=("cinnamon" "xfce" "gnome" "kde" "lxde")
+			select opt in "${options[@]}"; do
+				case $opt in
+					cinnamon ) echo "Installing cinnamon" ; pacman --noconfirm -S cinnamon ; cp /etc/skel/.xinitrc ~ ; echo "exec cinnamon-session" >> ~/.xinitrc ; echo "Completed" ; break ;;
+					xfce ) echo "Installing xfce" ; pacman --noconfirm -S xfce4 ; cp /etc/skel/.xinitrc ~ ; echo "exec xfce4-session" >> ~/.xinitrc ; echo "Completed" ; break ;;
+					gnome ) echo "Installing gnome" ; pacman --noconfirm -S gnome ; cp /etc/skel/.xinitrc ~ ; echo "exec gnome-session" >> ~/.xinitrc ; echo "Completed" ; break ;;
+					kde ) echo "Installing kde" ; pacman --noconfirm -S kdebase ; cp /etc/skel.xinitrc ~ ; echo "exec startkde" >> ~/.xinitrc ; echo "Completed" ; break ;;
+					lxde ) echo "Installing lxde" ; pacman --noconfirm -S lxde ; cp /etc/skel/.xinitrc ~ ; echo "exec startlxde" >> ~/.xinitrc ; echo "Completed" ;;
+				esac
+			done ; echo "Done with installing a Desktop, upon login, execute startx" ; break ;;
+        No ) echo "Moving on" ;;
     esac
 done
 
-echo "installing xorg and synaptics drivers"
-sleep 2
-pacman --noconfirm -S mesa xorg-server xorg-xinit xf86-input-synaptics xf86-input-mouse
+## Arch User Repository helper installer
 
-echo "Choose your graphics\nNOTE: The proprietary drivers are usually for newer gpus. If you are using an old gpu, pls. go with the opensource driver"
+echo -e "Do you want to enable AUR helper (yaourt) ?\nKeep in mind this will require base-devel"
+select yn in "Yes" "No"
+	case $yn in
+		Yes ) pacman --noconfirm -S base-devel wget ; wget https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz ; tar xzf package-query.tar.gz ; cd package-query ; makepkg --noconfirm -s -i ; cd .. ; wget https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz ; tar xzf yaourt ; cd yaourt ; makepkg --noconfirm -s -i ; cd .. ; echo "done" ; break ;;
+		No ) echo "Ok, suite yourself.." ;;
+	esac
+done
+
+## infinality installer
+echo "Do you want better font rendering with Infinality?" 
+select yn in "Yes" "No"
+	case $yn in
+		Yes ) yaourt --noconfirm -S freetype2-infinality fontconfig-infinality ; echo "choose a style" ; infctl setstyle ;
+				echo "Done" ; break ;;
+		No ) echo "Fine.." ;;
+	esac
+done
+
+## install graphics driver
+echo "Would you like to install graphics driver?"
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes ) sleep 1 ; break ;;
+		No ) exit ;;
+	esac
+done
+
+echo -e "Choose your graphics\nNOTE: The proprietary drivers are usually for newer gpus. If you are using an old gpu, pls. go with the opensource driver"
 options=("intel" "amd" "nvidia")
 select opt in "${options[@]}"; do
 	case $opt in
@@ -34,39 +71,4 @@ select opt in "${options[@]}"; do
 	esac
 done
 
-## Desktop environment installer
-
-echo "Choose a Desktop Environtment"
-options=("cinnamon" "xfce" "gnome" "kde" "lxde")
-select opt in "${options[@]}"; do
-	case $opt in
-		cinnamon ) echo "Installing cinnamon" ; pacman --noconfirm -S cinnamon ; cp /etc/skel/.xinitrc ~ ; echo "exec cinnamon-session" >> ~/.xinitrc ; echo "Completed" ; break ;;
-		xfce ) echo "Installing xfce" ; pacman --noconfirm -S xfce4 ; cp /etc/skel/.xinitrc ~ ; echo "exec xfce4-session" >> ~/.xinitrc ; echo "Completed" ; break ;;
-		gnome ) echo "Installing gnome" ; pacman --noconfirm -S gnome ; cp /etc/skel/.xinitrc ~ ; echo "exec gnome-session" >> ~/.xinitrc ; echo "Completed" ; break ;;
-		kde ) echo "Installing kde" ; pacman --noconfirm -S kdebase ; cp /etc/skel.xinitrc ~ ; echo "exec startkde" >> ~/.xinitrc ; echo "Completed" ; break ;;
-		lxde ) echo "Installing lxde" ; pacman --noconfirm -S lxde ; cp /etc/skel/.xinitrc ~ ; echo "exec startlxde" >> ~/.xinitrc ; echo "Completed" ;;
-	esac
-done
-
-echo "Done with installing a Desktop, upon login, execute startx"
-
-## Arch User Repository helper installer
-
-echo -e "Do you want to enable AUR helper (yaourt) ?\nKeep in mind this will require base-devel"
-select yn in "Yes" "No"
-	case $yn in
-		Yes ) pacman --noconfirm -S base-devel wget ; wget https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz ; tar xzf package-query.tar.gz ; cd package-query ; makepkg --noconfirm -s -i ; cd .. ; wget https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz ; tar xzf yaourt ; cd yaourt ; makepkg --noconfirm -s -i ; cd .. ; echo "done" ; break ;;
-		No ) echo "Ok, suite yourself.." ;;
-	esac
-done
-## infinality installer
-
-echo "Do you want better font rendering with Infinality?"
-select yn in "Yes" "No"
-	case $yn in
-		Yes ) yaourt --noconfirm -S freetype2-infinality fontconfig-infinality ; echo "choose a style" ; infctl setstyle ; break ;;
-		No ) echo "Fine.." ;;
-	esac
-done
-echo "Done with settings"
-
+echo "Done"
